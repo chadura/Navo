6426363fae21038970f955bcf80778903ae448c5
sowndharyan043 <sowndharyan@chadura.com>
2024-03-04 19:30:46 +0530

    Added Settings icon need to add hidden div to change the theme of browser

--- new_navo/navo/src/components/HomeBrowser.vue
+++ new_navo/navo/src/components/HomeBrowser.vue
@@ -1,12 +1,15 @@
 <template>
   <TopBar />
   <div class="relative bg-blue-400 z-0 dark:bg-gray-700 dark:text-white">
+    <button class="material-symbols-outlined absolute right-4 top-4">
+      settings
+    </button>
     <div class="flex flex-col items-center h-screen justify-center gap-y-5">
       <h1 class="text-2xl font-sans">NAVO</h1>
       <div class="flex bg-white w-[50%] rounded-lg relative">
         <input
           type="search"
-          class="w-[90%] rounded-lg py-4 px-2 focus:"
+          class="w-[90%] rounded-lg py-4 px-2 dark:text-black"
           placeholder="What you are looking for?"
           name=""
           id=""
